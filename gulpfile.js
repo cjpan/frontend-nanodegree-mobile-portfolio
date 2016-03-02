bbcfedf8a4fb24dc0d2cc234195381c69fa1f888
cjpan <chengjie.pan@gmail.com>
2016-03-02 23:41:14 +0800

    gulp files for the project

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,24 @@
 var gulp = require('gulp');
+var minifyHTML = require('gulp-minify-html');
 var uglify = require('gulp-uglify');
 var minifyCSS = require('gulp-minify-css');
 var imagemin = require('gulp-imagemin');
+
+gulp.task('html', function () {
+	gulp.src('./*.html')
+	.pipe(minifyHTML({
+		comments: true,
+		spare: true
+	}))
+	.pipe(gulp.dest('dist'));
+
+	gulp.src('views/*.html')
+	.pipe(minifyHTML({
+		comments: true,
+		spare: true
+	}))
+	.pipe(gulp.dest('dist/views'));
+});
 
 gulp.task('script', function () {
 	gulp.src('js/*.js')
@@ -38,12 +55,15 @@ gulp.task('images', function() {
 });
 
 gulp.task('auto', function() {
+	gulp.watch('./*.html', ['html']);
+	gulp.watch('views/*.html', ['html']);
 	gulp.watch('js/*.js', ['script']);
 	gulp.watch('views/js/*.js', ['script']);
 	gulp.watch('css/*.css', ['css']);
 	gulp.watch('views/css/*.css', ['css']);
 	gulp.watch('img/*.*', ['images']);
 	gulp.watch('views/images/*.*', ['images']);
+
 });
 
-gulp.task('default', ['script', 'css', 'images', 'auto']);
+gulp.task('default', ['html', 'script', 'css', 'images', 'auto']);
